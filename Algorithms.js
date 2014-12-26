51f67ff65ae3e86168ed94233f29922fedc834de
Yogesh Singh <ysingh@gainsight.com>
2014-12-26 23:48:45 +0530

    IN-1.2: Added duplicate function

--- Algorithms.js
+++ Algorithms.js
@@ -2,37 +2,71 @@
 	Algorithms = function() {
 
 	}
-
-	var removeDuplicatesFromString = function(arg) {
-		var obj = {};
-		var finalStr = '';
-		var finalArray = [];
-		for(var i = 0; i < arg.length; i++) {
-			if(typeof obj['' + arg[i]] === 'undefined') {
-				obj['' + arg[i]] = true;
-				typeof(arg) === "string" ? finalStr += arg[i] : finalArray.push(arg[i]);
-			}
+	var convertToType = function (oArray, type) {
+		var returnValue = oArray;
+		var originalValue = oArray.join('');
+		switch(type) {
+			case "number": returnValue = parseInt(originalValue);
+				break;
+			case "string": returnValue =  '' + originalValue;
+				break;
 		}
-		var returnValue = typeof(arg) === "string" ? finalStr : finalArray;
+		return returnValue;
+	}
+
+	var convertToArray = function (arg) {
+		var type = typeof(arg);
+		var returnValue = arg;
+		if(type === 'number' || type === 'string') {
+			returnValue = ('' + arg).split('');
+		}
 		return returnValue;
 	}
 
 	Algorithms.prototype.removeDuplicates = function() {
-		var arg = arguments[0];
-		var returnValue;
-		switch (typeof (arg)) {
-			case "string": 				
-				returnValue = removeDuplicatesFromString(arg);
-				break;
-			case "number":
-				returnValue = parseInt(removeDuplicatesFromString( '' + arg));
-				break;
-			case "object":
-				if(Array.isArray(arg)) {
-					returnValue = removeDuplicatesFromString(arg);	
-				}				
-				break;
-		}
-		return returnValue;
+		var arg = convertToArray(arguments[0]);
+		var sObject = {};
+		var finalArray = [];
+		arg.forEach(function(entry) {
+			if(typeof sObject[entry] === 'undefined') {
+				sObject[entry] = true;
+				finalArray.push(entry);
+			}
+		});
+		return convertToType(finalArray, typeof(arguments[0]));
+	}
+
+	Algorithms.prototype.maxOccurredCharacter = function () {
+		var arg = convertToArray(arguments[0]);
+		var sObject = {};
+		var max = 0;
+		var index;
+		arg.forEach(function(entry) {
+			if(typeof sObject[entry] === 'undefined') {
+				sObject[entry] = 1;
+			}
+			else {
+				sObject[entry] += 1;	
+			}
+			if(sObject[entry] > max) {
+				max = sObject[entry];
+				index = entry;
+			}
+		});
+		return index;
+	}
+	Algorithms.prototype.getDuplicates = function () {
+		var arg = convertToArray(arguments[0]);
+		var sObject = {};
+		var finalArray = [];
+		arg.forEach(function(entry) {
+			if(typeof sObject[entry] === 'undefined') {
+				sObject[entry] = true;
+			}
+			else if (sObject[entry] && finalArray.indexOf(entry) == -1) {
+				finalArray.push(entry);
+			}
+		});
+		return finalArray;
 	}
 }());
--- README.md
+++ README.md
@@ -7,10 +7,13 @@ JavaScript library to solve algorithms
 var obj = new Algorithms();
 
 obj.removeDuplicates('112233');
+
 "123"
 
 obj.removeDuplicates([1, 2, 1, 2]);
+
 [1, 2]
 
 obj.removeDuplicates(11122334);
+
 1234
