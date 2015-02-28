067428e8cecb243bfc6a4d85c44ab63c9146b68e
Yogesh Singh <ysingh@gainsight.com>
2015-03-01 01:53:18 +0530

    more code

--- tree.js
+++ tree.js
@@ -33,6 +33,35 @@ function postOrder(root) {
 	console.log(root.data);
 }
 
+Array.prototype.enQueue = function() {
+	this.push(arguments[0]);
+};
+
+Array.prototype.deQueue = function() {
+	if(this.length > 0) {
+		return this.shift();
+	}
+};
+
+Array.prototype.isEmptyQueue = function() {
+	return this.length == 0;
+};
+
+function levelOrder(root, queue) {
+	queue.enQueue.call(queue, root);
+	while(!queue.isEmptyQueue.call(queue)) {
+		var tmp = queue.deQueue.call(queue);
+		console.log(tmp.data);
+		if(tmp.left != null) {
+			queue.enQueue.call(queue, tmp.left);
+		}
+		if (tmp.right != null) {
+			queue.enQueue.call(queue, tmp.right);
+		}
+	}
+}
+
+
 function findPath(root, path, value) {
 	if(root == null) {
 		return false;
@@ -79,6 +108,10 @@ preOrder(root);
 
 console.log("Postorder:")
 postOrder(root);
+
+path = [];
+console.log("LevelOrder : ");
+levelOrder(root, path);
 
 var path = [];
 if(findPath(root, path, 6)) {
@@ -147,7 +180,71 @@ function heightOfTree(root) {
 	if(root == null) {
 		return 0;
 	}
-	return Math.max(1 + heightOfTree(root.left), 1 + heightOfTree(root.right));
+	return 1 + Math.max(heightOfTree(root.left), heightOfTree(root.right));
 }
 
 heightOfTree(root);
+
+var root1 = new newNode(1);
+root1.left = new newNode(2);
+root1.right = new newNode(3);
+root1.left.left = new newNode(4);
+root1.left.right = new newNode(5);
+root1.right.left = new newNode(6);
+root1.right.right = new newNode(7);
+
+var root2 = new newNode(1);
+root2.left = new newNode(2);
+root2.right = new newNode(3);
+root2.left.left = new newNode(4);
+root2.left.right = new newNode(5);
+root2.right.left = new newNode(6);
+root2.right.right = new newNode(7);
+
+function isIdentical(root1, root2) {
+	if (root1 == null && root2 == null) { return true };
+
+	if(root1.data == root2.data && 
+		isIdentical(root1.left, root2.left) &&
+		isIdentical(root1.right, root2.right)) { return true };
+
+	return false;
+}
+
+console.log("Identical: " + isIdentical(root1, root2));
+
+function deleteTree(root) {
+	if (root == null) { return };
+	deleteTree(root.left);
+	delete root.left;
+	delete root.right;
+	delete root.data;
+	deleteTree(root.right);
+}
+
+//deleteTree(root);
+
+//If you are given two traversal sequences, 
+//can you construct the binary tree - 
+
+// If one of the traversal is inOrder
+
+path = [];
+
+function rootToLeafPath(root, path) {
+	if(root == null) {
+		return;
+	}
+	path.push(root.data);
+	if(root.left == null && root.right == null) {
+		console.log(path);
+	}
+	rootToLeafPath(root.left, path);
+	rootToLeafPath(root.right, path);
+	path.pop()
+}
+
+rootToLeafPath(root, path);
+
+
+
